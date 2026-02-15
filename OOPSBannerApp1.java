f437bf0d87ba7dfbe2fb15a203f419006850e23c
aarohvikramsingh <aarohvikramsingh@gmail.com>
2026-02-16 01:10:38 +0530

    print oops

---
+++ OOPSBannerApp1.java
@@ -0,0 +1,24 @@
+
+/**
+ * OOPSBannerApp is a simple Java program that prints "OOPS" 
+ * to the console. This demonstrates the basic structure 
+ * of a Java application.
+ *
+ * @author Aaroh
+ * @version 1.0
+ */
+
+public class OOPSBannerApp1 {
+
+    /**
+     * Main method - Entry point of the Java application.
+     * It prints the literal text "OOPS" to the console.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Print statement to display OOPS
+        System.out.println("OOPS");
+    }
+}
