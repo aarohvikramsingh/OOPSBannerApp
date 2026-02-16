cff8e15148ec520bd7cfe028bd4037ae947882fb
aarohvikramsingh <aarohvikramsingh@gmail.com>
2026-02-16 13:28:49 +0530

    Co-authored-by: Abhijeet Padhy <Abhijeet-980@users.noreply.github.com>

---
+++ OOPSBanner.java
@@ -0,0 +1,29 @@
+public class OOPSBanner {
+    public static void main(String[] args) {
+
+        String O1, O2, O3, O4, O5, O6, O7;
+
+        // Line 1
+        O1 = "  *****  " + "   *****  " + "   ****** " + "  ****** ";
+        // Line 2
+        O2 = " *     * " + "  *     * " + "  *     * " + " *     * ";
+        // Line 3
+        O3 = " *     * " + "  *     * " + "  *     * " + " *       ";
+        // Line 4
+        O4 = " *     * " + "  *     * " + "  ******  " + "  *****  ";
+        // Line 5
+        O5 = " *     * " + "  *     * " + "  *       " + "       * ";
+        // Line 6
+        O6 = " *     * " + "  *     * " + "  *       " + " *     * ";
+        // Line 7
+        O7 = "  *****  " + "   *****  " + "  *       " + "  *****  ";
+
+        System.out.println(O1);
+        System.out.println(O2);
+        System.out.println(O3);
+        System.out.println(O4);
+        System.out.println(O5);
+        System.out.println(O6);
+        System.out.println(O7);
+    }
+}
